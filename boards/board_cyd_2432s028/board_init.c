5f602b61737aa47c7eb4de7c74f238322a0e2a0b
zombodotcom <tommysciano@gmail.com>
2026-05-09 14:12:56 -0500

    board_cyd: wire ILI9341 panel through display_panel_iface

--- boards/board_cyd_2432s028/board_init.c
+++ boards/board_cyd_2432s028/board_init.c
@@ -1,11 +1,32 @@
 // boards/board_cyd_2432s028/board_init.c
 #include "board_pins.h"
+#include "ili9341_panel.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
 
 static const char *TAG = "board";
 
-const display_panel_iface_t *board_get_panel(void) { return NULL; }   // T11
+static const ili9341_pins_t s_lcd_pins = {
+    .sck  = BOARD_LCD_SCK,
+    .mosi = BOARD_LCD_MOSI,
+    .miso = BOARD_LCD_MISO,
+    .cs   = BOARD_LCD_CS,
+    .dc   = BOARD_LCD_DC,
+    .rst  = BOARD_LCD_RST,
+    .bl   = BOARD_LCD_BL,
+};
+
+static esp_err_t cyd_panel_init(void) { return ili9341_init(&s_lcd_pins); }
+
+static const display_panel_iface_t s_panel = {
+    .width      = BOARD_LCD_HRES,
+    .height     = BOARD_LCD_VRES,
+    .swap_bytes = true,
+    .init       = cyd_panel_init,
+    .draw_rows  = ili9341_draw_rows,
+};
+
+const display_panel_iface_t *board_get_panel(void) { return &s_panel; }
 const panel_touch_iface_t   *board_get_touch(void) { return NULL; }   // T15
 const led_status_iface_t    *board_get_led(void)   { return NULL; }   // T16
 
